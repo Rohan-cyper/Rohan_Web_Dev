29dc3eeff37d4faf15382c5c0e079f58521c5373
Rohan <rohan9058183753@gmail.com>
2021-04-02 10:44:10 +0530

    New Upload

---
+++ script1.js
@@ -0,0 +1,11 @@
+var x=0;
+function myFunction() {
+    x++;
+    document.getElementById("college").src = 'sscbs.jpg';        
+    console.log(x);
+}
+function myFun2() {
+    x++;
+    document.getElementById("college").src = 'sscbs2.jpg';
+    console.log(x);
+}
